c9aa1016ec62b36f7968192d501548d2c876c7ee
laijiaaa1 <laijiaaa1@gmail.com>
2023-08-19 01:49:05 +0800

    0818

--- 0816/ViewController.swift
+++ 0816/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageViewview: UIImageView!
+    var imageNumber = 0
+    var messageNumber = 0
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,25 +21,48 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonView(_ sender: UIButton) {
-        let awscomemessage = "You Are Awesome!"
-        
-        if messageLabel.text == awscomemessage {
-            messageLabel.text = "You Are Great!"
-            messageLabel.textColor = UIColor.black
-            messageLabel.textAlignment = .center
-            imageViewview.image = UIImage(named: "image0")
-        }else if messageLabel.text == "You Are Great!"{
-            messageLabel.text = "You Are cat!"
-            messageLabel.textColor = UIColor.red
-            messageLabel.textAlignment = .center
-            imageViewview.image = UIImage(named: "cat")
-            
-        }else{
-            messageLabel.text = awscomemessage
-            messageLabel.textColor = UIColor.blue
-            messageLabel.textAlignment = .center
-            imageViewview.image = UIImage(named: "image1")
+        let messages = ["Nothing is impossible",
+                        "Do the best you can",
+                        "Don't let yesterday take up too much of today",
+                        "You learn more from failure than from success",
+                        "It's never too late to be what you might've been"]
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
         }
+        
+        let imageName = "image\(imageNumber)"
+        imageViewview.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 6{
+            imageNumber = 0
+        }
+        
+        
+        
+        
+        
+        
+//        let awscomemessage = "You Are Awesome!"
+//
+//        if messageLabel.text == awscomemessage {
+//            messageLabel.text = "You Are Great!"
+//            messageLabel.textColor = UIColor.black
+//            messageLabel.textAlignment = .center
+//            imageViewview.image = UIImage(named: "image0")
+//        }else if messageLabel.text == "You Are Great!"{
+//            messageLabel.text = "You Are cat!"
+//            messageLabel.textColor = UIColor.red
+//            messageLabel.textAlignment = .center
+//            imageViewview.image = UIImage(named: "cat")
+//
+//        }else{
+//            messageLabel.text = awscomemessage
+//            messageLabel.textColor = UIColor.blue
+//            messageLabel.textAlignment = .center
+//            imageViewview.image = UIImage(named: "image1")
+//        }
     }
     
     
