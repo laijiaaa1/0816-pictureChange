4e4f2cbee9fd86195e0ec65ece5fa32fe5c82ecf
laijiaaa1 <laijiaaa1@gmail.com>
2023-08-20 13:29:43 +0800

    0820

--- 0816/ViewController.swift
+++ 0816/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewview: UIImageView!
     var imageNumber = 0
     var messageNumber = 0
-   
+    var imagetotal = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,19 +26,26 @@ class ViewController: UIViewController {
                         "Don't let yesterday take up too much of today",
                         "You learn more from failure than from success",
                         "It's never too late to be what you might've been"]
-        messageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }
         
-        let imageName = "image\(imageNumber)"
-        imageViewview.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 6{
-            imageNumber = 0
-        }
+        messageLabel.text = messages[Int.random(in:0...messages.count-1)]
+        imageViewview.image = UIImage(named: "image\(Int.random(in:0...imagetotal))")
+    
         
+        
+        
+//        messageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
+//        }
+//
+//        let imageName = "image\(imageNumber)"
+//        imageViewview.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 6{
+//            imageNumber = 0
+//        }
+//
         
         
         
