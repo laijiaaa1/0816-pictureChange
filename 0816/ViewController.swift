eae8ccc77c221f5c6c5b46d296d9a263bae69254
laijiaaa1 <laijiaaa1@gmail.com>
2023-08-21 11:47:11 +0800

    0821

--- 0816/ViewController.swift
+++ 0816/ViewController.swift
@@ -12,51 +12,97 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageViewview: UIImageView!
-    var imageNumber = 0
-    var messageNumber = 0
+    @IBOutlet weak var playSoundSwitch: UISwitch!
+    var imageNumber = -1
+    var messageNumber = -1
+    var soundNumber = -1
     var imagetotal = 5
+    var numberOfSounds = 3
     var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  
     }
 
+    
+    func playSound(name: String){
+        if let sound = NSDataAsset(name: name){
+                    do{
+                        try audioPlayer = AVAudioPlayer(data: sound.data)
+                        audioPlayer.play()
+                    }catch{
+                        print("😡 ERROR: \(error.localizedDescription)could not read data from file sound0")
+                    }
+                }else{
+                    print("ERROR: could not read data from file sound0")
+                }
+    }
+    func nonRepeatingRandom (originalNumber: Int, upperLimit: Int) -> Int{
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...upperLimit)
+        }while originalNumber == newNumber
+        return newNumber
+    }
+    
     @IBAction func buttonView(_ sender: UIButton) {
         let messages = ["Nothing is impossible",
                         "Do the best you can",
                         "Don't let yesterday take up too much of today",
                         "You learn more from failure than from success",
                         "It's never too late to be what you might've been"]
-
-        var newMessageNumber: Int
-        repeat {
-            newMessageNumber = Int.random(in: 0...messages.count-1)
-        }while messageNumber == newMessageNumber
-        messageNumber = newMessageNumber
+        
+        messageNumber = nonRepeatingRandom (originalNumber: messageNumber, upperLimit: messages.count-1)
         messageLabel.text = messages[messageNumber]
         
-        var newImageNUmber: Int
-        repeat {
-            newImageNUmber = Int.random(in: 0...imagetotal-1)
-        }while imageNumber == newImageNUmber
-        imageNumber = newImageNUmber
+       
+        imageNumber = nonRepeatingRandom (originalNumber: imageNumber, upperLimit: imagetotal-1)
         imageViewview.image = UIImage(named: "image\(imageNumber)")
         
-        if let sound = NSDataAsset(name: "sound0"){
-            do{
-                try audioPlayer = AVAudioPlayer(data: sound.data)
-                audioPlayer.play()
-            }catch{
-                print("😡 ERROR: \(error.localizedDescription)could not read data from file sound0")
-            }
-        }else{
-            print("ERROR: could not read data from file sound0")
+        
+        soundNumber = nonRepeatingRandom(originalNumber: soundNumber, upperLimit: numberOfSounds-1)
+        if playSoundSwitch.isOn{
+            playSound(name: "sound\(soundNumber)")
+            
         }
-        
-        
-        
-        
+    }
+    
+    @IBAction func playSoundToggled(_ sender: UISwitch){
+        if !sender.isOn && audioPlayer != nil {
+            audioPlayer.stop()
+        }
+    }
+    
+////     5.0
+//        var newMessageNumber: Int
+//        repeat {
+//            newMessageNumber = Int.random(in: 0...messages.count-1)
+//        }while messageNumber == newMessageNumber
+//        messageNumber = newMessageNumber
+//        messageLabel.text = messages[messageNumber]
+//
+//        var newImageNUmber: Int
+//        repeat {
+//            newImageNUmber = Int.random(in: 0...imagetotal-1)
+//        }while imageNumber == newImageNUmber
+//        imageNumber = newImageNUmber
+//        imageViewview.image = UIImage(named: "image\(imageNumber)")
+//        if let sound = NSDataAsset(name: "sound0"){
+//            do{
+//                try audioPlayer = AVAudioPlayer(data: sound.data)
+//                audioPlayer.play()
+//            }catch{
+//                print("😡 ERROR: \(error.localizedDescription)could not read data from file sound0")
+//            }
+//        }else{
+//            print("ERROR: could not read data from file sound0")
+//        }
+    
+    
+    
+    
 //       4.0
 //        var newMessage = messages[Int.random(in: 0...messages.count-1)]
 //        while messageLabel.text == newMessage{
@@ -66,14 +112,14 @@ class ViewController: UIViewController {
 //        messageLabel.text = newMessage
 //        imageViewview.image = UIImage(named: "image\(Int.random(in:0...imagetotal))")
 //
-        
-        
+    
+    
 //       3.0
 //        messageLabel.text = messages[Int.random(in:0...messages.count-1)]
 //        imageViewview.image = UIImage(named: "image\(Int.random(in:0...imagetotal))")
 //
-        
-        
+    
+    
 //       2.0
 //        messageLabel.text = messages[messageNumber]
 //        messageNumber += 1
@@ -88,10 +134,10 @@ class ViewController: UIViewController {
 //            imageNumber = 0
 //        }
 //
-        
-        
-        
-        
+    
+    
+    
+    
 //        1.0
 //        let awscomemessage = "You Are Awesome!"
 //
@@ -112,8 +158,5 @@ class ViewController: UIViewController {
 //            messageLabel.textAlignment = .center
 //            imageViewview.image = UIImage(named: "image1")
 //        }
-    }
-    
-    
 }
 
